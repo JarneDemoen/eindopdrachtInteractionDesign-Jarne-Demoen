bd3f05f86f5808b6ec720cb9a8064bef9e57d597
Jarne Demoen <jarne.demoen@student.howest.be>
2021-10-20 19:05:48 +0200

    api request lukt niet

--- script/app.js
+++ script/app.js
@@ -18,10 +18,23 @@ function dropdownContent() {
     }
 }
 
+function filterDataEurope(jsonObject) {
+    console.log(jsonObject)
+}
+
+let getData = async () => {
+    const ENDPOINT = `https://queue-times.com/nl/parks.json`;
+
+    // Met de fetch API proberen we de data op te halen.
+    const request = await fetch(`${ENDPOINT}`);
+    const data = await request.json();
+    console.log("getAPI data", data);
+}
 
 function init() {
     console.log('DOM Geladen');
     dropdownContent();
+    getData();
 }
 
 document.addEventListener('DOMContentLoaded', init);
