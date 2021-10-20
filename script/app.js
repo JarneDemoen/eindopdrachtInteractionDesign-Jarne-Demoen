6f6f2c2047777fb91de5783dbbe39115487627f4
Jarne Demoen <jarne.demoen@student.howest.be>
2021-10-20 16:44:04 +0200

    Bezig met site

--- script/app.js
+++ script/app.js
@@ -1,8 +1,27 @@
 'use strict';
 
 
+var dropdown = document.getElementsByClassName("dropdown-Country");
+var i;
+
+function dropdownContent() {
+    for (i = 0; i < dropdown.length; i++) {
+        dropdown[i].addEventListener("click", function () {
+            this.classList.toggle("active");
+            var dropdownContent = this.nextElementSibling;
+            if (dropdownContent.style.display === "block") {
+                dropdownContent.style.display = "none";
+            } else {
+                dropdownContent.style.display = "block";
+            }
+        });
+    }
+}
+
+
 function init() {
     console.log('DOM Geladen');
+    dropdownContent();
 }
 
 document.addEventListener('DOMContentLoaded', init);
