6e06600bdc0e904c41b6fb601c1383f61b055004
Jarne Demoen <jarne.demoen@student.howest.be>
2021-10-27 20:44:36 +0200

    Animaties moeten nog komen

--- script/app.js
+++ script/app.js
@@ -4,6 +4,23 @@
 var dropdown = document.getElementsByClassName("dropdown-Country");
 var i;
 let myCountries = ['Belgium', 'France', 'Netherlands', 'Germany'];
+let menubtn;
+let sidebar;
+let maincontent;
+
+function ListenToClickMenu() {
+    menubtn.addEventListener('click', function () {
+        sidebar = document.querySelector('.sidenavigation')
+        maincontent = document.querySelector('.ride')
+        if (sidebar.style.display == 'block') {
+            sidebar.style.display = 'none'
+            maincontent.style.opacity = '100%'
+        } else {
+            sidebar.style.display = 'block'
+            maincontent.style.opacity = '30%'
+        }
+    })
+}
 
 function dropdownContent() {
     for (i = 0; i < dropdown.length; i++) {
@@ -17,6 +34,10 @@ function dropdownContent() {
             }
         });
     }
+
+    if (menubtn) {
+        ListenToClickMenu();
+    }
 }
 
 function fillData(data) {
@@ -27,12 +48,15 @@ function fillData(data) {
     let sidenavcountrystring = ``
     for (let i = 0; i < myCountries.length; i++) {
         let country = myCountries[i]
-        if (country == 'Netherlands'){
+        if (country == 'Netherlands') {
             country = 'The Netherlands'
         }
         sidenavcountrystring += `<button class="dropdown-Country">${country}
         <i class="fa fa-caret-down">&#9660</i>
       </button>`;
+        if (country == 'The Netherlands') {
+            country = 'Netherlands'
+        }
         sidenavcountrystring += `<div class="dropdown-container">`;
         for (let j = 0; j < data.length; j++) {
             let parkobj = data[j]
@@ -65,9 +89,8 @@ function filterDataEurope(jsonObject) {
     fillData(filteredDataArray);
 }
 
-let getData = async () => { 
-    const ENDPOINT = `https://cors-anywhere.herokuapp.com/https://queue-times.com/nl/parks.json`;
-    // const ENDPOINT = `https://queue-times.com/nl/parks.json`;
+let getData = async () => { // const ENDPOINT = `https://cors-anywhere.herokuapp.com/https://queue-times.com/nl/parks.json`;
+    const ENDPOINT = `https://queue-times.com/nl/parks.json`;
 
     // Met de fetch API proberen we de data op te halen.
     const request = await fetch(`${ENDPOINT}`, {dataType: 'jsonp'});
@@ -77,6 +100,8 @@ let getData = async () => {
 
 function init() {
     console.log('DOM Geladen');
+    menubtn = document.querySelector('.menu__symbol');
+    console.log('MENUBTN', menubtn)
     getData();
 }
 
