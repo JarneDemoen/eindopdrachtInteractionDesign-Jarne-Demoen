972e1987df21b69e8861fa7ee8e047e3f4e7c20e
Jarne Demoen <jarne.demoen@student.howest.be>
2021-11-01 18:22:51 +0100

    img source gewijzigd om github pages te doen werkn

--- script/app.js
+++ script/app.js
@@ -31,7 +31,7 @@ function fillContentWithFavorites() {
         if (ride.name == 'Cécémel Ice Rink') {
             ride.name = 'Cecemel Ice Rink'
         }
-        let imgSource = `/img/${
+        let imgSource = `/eindopdrachtInteractionDesign-Jarne-Demoen/img/${
             modifyName(ride.parkname)
         }/${
             modifyName(ride.name)
